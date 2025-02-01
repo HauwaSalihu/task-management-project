9d51c33c32375d9291210d42ac9daee3d1229e8b
HauwaSalihu <hauwa.salihu@aun.edu.ng>
2025-02-01 19:50:10 +0100

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,7 @@ import { Routes, Route } from "react-router";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
-<<<<<<< HEAD
-import MainLayout from "./layout/MainLayout";
-=======
 import MainLayout from "./layout/MainLayout.jsx";
->>>>>>> 1f894fe58248043efee6a9e815268bd60dbaf19d
 import Task from "./pages/Task";
 import ProtectedRoute from "./layout/ProtetedRoute.jsx";
 
